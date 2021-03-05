9a07604aa86c9885468b016abdbc613d61a787f4
Brice Burgess <nesta@iceburg.net>
2021-03-05 13:27:11 -0700

    stub lookup function

--- lib/shell-helpers.sh
+++ lib/shell-helpers.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck ignore-variable-pattern=__
 lib/read/paths()(
   IFS=:; set -o noglob
   for p in $1; do
@@ -10,4 +11,16 @@ lib/require/dir(){
   for d in "$@"; do
     [ -d "$d" ] || die "missing directory: $d"
   done
+}
+
+lib/lookup(){
+  log "looking for '$1' $2"
+  for p in "${__SEARCH_PATHS[@]}"; do
+    [ -e "$p/$1" ] && {
+      echo "$p/$1"
+      return 0
+    }
+    log "    tried ${p/$ANYCI_ROOT/<anyci workspace>}/$1"
+  done
+  return 1
 }
