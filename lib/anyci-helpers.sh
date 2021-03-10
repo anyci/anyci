b0074fab1a590156d6c281490f7cce3ceaccf73f
Brice Burgess <nesta@iceburg.net>
2021-03-10 13:35:30 -0700

    add lib/exec helper

--- lib/anyci-helpers.sh
+++ lib/anyci-helpers.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-lib/read/paths()(
-  IFS=:; set -o noglob
-  for p in $1; do
-    echo "$p"
-  done
-)
 
-lib/require/dir(){
-  for d in "$@"; do
-    [ -d "$d" ] || die "missing directory: $d"
-  done
+lib/exec(){
+  $__lib_exec_initialized || {
+    export EXEC_IMAGE="${EXEC_IMAGE:-$ANYCI_IMAGE}"
+    export EXEC_DOCKER_FLAGS="-v,$ANYCI_ROOT:$ANYCI_ROOT:ro"
+
+    [ -n "$EXEC_IMAGE" ] || {
+      dlog "ANYCI_IMAGE not provided. Will build from Dockerfile..."
+      EXEC_DOCKERFILE="$(lib/lookup docker/Dockerfile)"
+      export EXEC_DOCKERFILE
+    }
+    __lib_exec_initialized=true
+  }
+
+  cd "$PROJECT_ROOT" || die "failed to enter PROJECT_ROOT"
+  "$ANYCI_ROOT/bin/exec" "$@"
 }
 
 lib/lookup(){
@@ -26,6 +31,19 @@ lib/lookup(){
   return 1
 }
 
+lib/read/paths()(
+  IFS=:; set -o noglob
+  for p in $1; do
+    echo "$p"
+  done
+)
+
+lib/require/dir(){
+  for d in "$@"; do
+    [ -d "$d" ] || die "missing directory: $d"
+  done
+}
+
 lib/set/search_paths(){
   __SEARCH_PATHS=()
   for p in $(lib/read/paths "$1"); do
@@ -34,3 +52,6 @@ lib/set/search_paths(){
   [ -n "$ANYCI_FAMILY" ] && __SEARCH_PATHS+=("$ANYCI_ROOT/family/$ANYCI_FAMILY")
   __SEARCH_PATHS+=("$ANYCI_ROOT/group/$ANYCI_GROUP")
 }
+
+export ANYCI_ROOT
+__lib_exec_initialized=false
