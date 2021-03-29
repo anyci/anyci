869ae5df755462cb5d379d5545482ecfd7816482
Brice Burgess <nesta@iceburg.net>
2021-03-29 17:18:17 -0400

    use ANYCI_STEP_NAME when logging

--- lib/log-helpers.sh
+++ lib/log-helpers.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-log(){ printf "# %-${__lpad:-17}s [${__ll:-INFO}] $*\n" "($__ENTRY)" >&2; }
+log(){ printf "# %-${__lpad:-17}s [${__ll:-INFO}] $*\n" "(${ANYCI_STEP_NAME:-$__ENTRY})" >&2; }
 dlog(){ __ll=debug log "$*"; }
 die(){ __ll=ERR log "$*"; exit 1; }
